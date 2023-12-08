5b808ebc289627f8643316ac33cc9465b427bcf2
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-12-08 12:25:39 +0800

    Remove celery specific components

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,18 @@
-FROM ghcr.io/irfanhakim-as/dim:0.0.1-stable
+FROM ghcr.io/irfanhakim-as/dim:0.0.3-stable-r1
 
 ENV APP="base"
 
 # ================= DO NOT EDIT BEYOND THIS LINE =================
-
-COPY celery/default/* /etc/default/
-
-COPY celery/init.d/* /etc/init.d/
-
-COPY entrypoint.sh /
 
 COPY requirements.txt /tmp/
 
 RUN pip3 install --no-cache-dir -r /tmp/requirements.txt \
     && rm /tmp/requirements.txt
 
-COPY base/*.py $APP/
+COPY entrypoint.sh /
+
+COPY base/*.py ${APP}/
 
 COPY lib/*.py lib/
 
-COPY commands/*.py $APP/management/commands/
+COPY commands/*.py ${APP}/management/commands/
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,27 +4,27 @@ export APP_ROOT="base"
 
 # ================= DO NOT EDIT BEYOND THIS LINE =================
 
+if [ -f "/etc/init.d/celeryd" ] && [ -f "/etc/init.d/celerybeat" ]; then
+    BACKEND_SCHEDULER="celery"
+fi
+
 python3 manage.py makemigrations
 
 python3 manage.py migrate
 
-chmod -R 775 /$APP_ROOT \
-        /var/log/apache2
+chmod -R 775 /${APP_ROOT} /var/log/apache2
 
-chmod 755 /etc/init.d/celeryd \
-        /etc/init.d/celerybeat
-
-chmod 640 /etc/default/celeryd
-
-chown -R www-data: /$APP_ROOT /var/log/apache2
+chown -R www-data: /${APP_ROOT} /var/log/apache2
 
 python3 manage.py test
 
-if [ $? -eq 0 ]; then
+if [ ${?} -eq 0 ]; then
     python3 manage.py entrypoint &> /dev/null
-    
-    /etc/init.d/celeryd start && /etc/init.d/celerybeat start
-    
+
+    if [ "${BACKEND_SCHEDULER}" = "celery" ]; then
+        /etc/init.d/celeryd start && /etc/init.d/celerybeat start
+    fi
+
     apache2ctl -D FOREGROUND
 else
     tail -f /dev/null
