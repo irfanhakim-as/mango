ee03fc3232a2041c9a8eebd17e76a4855564377c
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-12-19 23:38:10 +0800

    Minor message update

--- commands/clean_data.py
+++ commands/clean_data.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from lib.post import clean_data
 
 class Command(BaseCommand):
-    help = "Update and clean the PostItem model with the latest relevant data"
+    help = "Update and clean the PostModel with the latest relevant data"
 
     def handle(self, *args, **options):
         clean_data()
