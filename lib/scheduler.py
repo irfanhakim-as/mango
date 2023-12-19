d24af62e00beab35678b6d8f3669c539d8de7e78
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-12-20 01:05:54 +0800

    Update default post_objects query

--- lib/scheduler.py
+++ lib/scheduler.py
@@ -19,10 +19,10 @@ def schedule_post(subject_object, **kwargs):
 #====================SCHEDULER: POST SCHEDULER====================#
 def post_scheduler(limit=POST_LIMIT, **kwargs):
     count = kwargs.get("count", random.randint(1, limit))
-    # get `count` number of PostSchedule objects that have not been posted, ordered by subject__published (oldest first)
-    pending_objects = PostSchedule.objects.filter(subject__post_id__isnull=True).order_by("subject__published")[:count]
-    # get all PostSchedule objects that have been posted, ordered by subject__published (oldest first)
-    updating_objects = PostSchedule.objects.filter(subject__post_id__isnull=False).order_by("subject__published")
+    # get `count` number of PostSchedule objects that have not been posted, ordered by id (ascending)
+    pending_objects = PostSchedule.objects.filter(subject__post_id__isnull=True).order_by("id")[:count]
+    # get all PostSchedule objects that have been posted, ordered by id (ascending)
+    updating_objects = PostSchedule.objects.filter(subject__post_id__isnull=False).order_by("id")
     # combine the two querysets
     post_objects = pending_objects | updating_objects
 
