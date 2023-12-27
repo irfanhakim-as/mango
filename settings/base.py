787cae693a1abc43415e9c84fd575f04b2ead704
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-12-28 02:37:48 +0800

    New data config settings

--- settings/base.py
+++ settings/base.py
@@ -18,7 +18,11 @@ COMPULSORY_SETTINGS.extend([
     "POST_LIMIT",
     "CELERY_BROKER_URL",
     "CELERY_RESULT_BACKEND",
-    "CELERY_TIMEZONE"
+    "CELERY_TIMEZONE",
+    "DATA_DIR",
+    "ACCOUNTS_DATA_FILE",
+    "FEEDS_DATA_FILE",
+    "SYNC_CONFIG",
 ])
 ORGANIC_POSTS = os.getenv("ORGANIC_POSTS", False) == "true"
 ACCOUNT_MODEL = os.getenv("ACCOUNT_MODEL", "base.MastodonAccount")
@@ -45,3 +49,22 @@ POST_LIMIT = int(os.getenv("POST_LIMIT", "0"))
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER", "redis://localhost:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_BACKEND", "redis://localhost:6379/0")
 CELERY_TIMEZONE = SCHEDULER_TIMEZONE
+
+
+##################################################################
+# Data Settings
+##################################################################
+
+DATA_DIR = os.getenv("DATA_DIR", os.path.join(BASE_DIR, "data"))
+ACCOUNTS_DATA_FILE = os.getenv("ACCOUNTS_DATA_FILE", os.path.join(DATA_DIR, "accounts.json"))
+FEEDS_DATA_FILE = os.getenv("FEEDS_DATA_FILE", os.path.join(DATA_DIR, "feeds.json"))
+SYNC_CONFIG = {
+    "accounts" : {
+        "model" : ACCOUNT_MODEL,
+        "data" : ACCOUNTS_DATA_FILE,
+    },
+    "feeds" : {
+        "model" : FEED_MODEL,
+        "data" : FEEDS_DATA_FILE,
+    },
+}
