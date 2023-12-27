a45abf99c05822e8a0742d2def138c46d4025277
irfanhakim-as <irfanhakim.as@yahoo.com>
2023-12-27 23:56:14 +0800

    Add update_accounts signal

--- base/signals.py
+++ base/signals.py
@@ -2,11 +2,14 @@ import logging
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from base.methods import (
+    get_active_accounts,
     get_post_model,
     message,
 )
+from lib.mastodon import update_account
 from lib.scheduler import schedule_post
 logger = logging.getLogger("base")
+AccountModel = get_active_accounts()
 PostModel = get_post_model()
 
 
@@ -21,3 +24,24 @@ def schedule_posts(sender, instance, created, **kwargs):
     else:
         log_message = message("LOG_EVENT", event='%s object "%s" already has a PostSchedule object "%s"' % (PostModel.__name__, instance, instance.postschedule_set.first().pk))
         logger.info(log_message)
+
+
+#====================MASTODON: UPDATE ACCOUNTS====================#
+@receiver(post_save, sender=AccountModel)
+def update_accounts(sender, instance, created, **kwargs):
+    params = dict(
+        access_token = getattr(instance, "access_token"),
+        api_base_url = getattr(instance, "api_base_url"),
+        is_bot = getattr(instance, "bot"),
+        is_discoverable = getattr(instance, "discoverable"),
+        display_name = getattr(instance, "display_name"),
+        fields = getattr(instance, "fields"),
+        is_locked = getattr(instance, "locked"),
+        note = getattr(instance, "note"),
+    )
+    # update mastodon account
+    account = update_account(**params)
+    if not account:
+        verbose_warning = "Mastodon account failed to be updated"
+        log_message = message("LOG_EXCEPT", exception=None, verbose=verbose_warning, object=instance)
+        logger.warning(log_message)
