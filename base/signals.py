9fd1e647581a2a953a3e640849e7c541af234dae
irfanhakim-as <irfanhakim.as@yahoo.com>
2024-01-05 18:19:44 +0800

    Obtain ScheduleModel dynamically

--- base/signals.py
+++ base/signals.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from base.methods import (
     get_account_model,
     get_post_model,
+    get_schedule_model,
     message,
 )
 from lib.mastodon import update_account
@@ -11,18 +12,21 @@ from lib.scheduler import schedule_post
 logger = logging.getLogger("base")
 AccountModel = get_account_model()
 PostModel = get_post_model()
+ScheduleModel = get_schedule_model()
 
 
 #====================POST: SCHEDULE POSTS====================#
 @receiver(post_save, sender=PostModel)
 def schedule_posts(sender, instance, created, **kwargs):
+    schedule_related_name = "%s_set" % ScheduleModel.__name__.lower()
+    schedule_obj = getattr(instance, schedule_related_name)
     # schedule object if it has not been scheduled yet
-    if not instance.postschedule_set.exists():
+    if not schedule_obj.exists():
         log_message = message("LOG_EVENT", event='Scheduling %s object "%s"' % (PostModel.__name__, instance))
         logger.info(log_message)
         schedule_post(instance)
     else:
-        log_message = message("LOG_EVENT", event='%s object "%s" already has a PostSchedule object "%s"' % (PostModel.__name__, instance, instance.postschedule_set.first().pk))
+        log_message = message("LOG_EVENT", event='%s object "%s" already has a %s object "%s"' % (PostModel.__name__, instance, ScheduleModel.__name__, schedule_obj.first().pk))
         logger.info(log_message)
 
 
