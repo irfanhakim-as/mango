f273c1f8dc181772e105674f5755aa93f0250b0f
irfanhakim-as <irfanhakim.as@yahoo.com>
2024-01-10 01:52:54 +0800

    Delete instead of schedule schedule_candidates if configured to not retry

--- base/post.py
+++ base/post.py
@@ -1,4 +1,5 @@
 import logging
+from django.conf import settings
 from base.methods import (
     get_values_list,
     message,
@@ -8,16 +9,27 @@ from lib.scheduler import schedule_post
 logger = logging.getLogger("base")
 
 
+#====================SETTINGS: GETATTR====================#
+RETRY_POST = getattr(settings, "RETRY_POST")
+
+
 #====================BASE: CLEAN DATA====================#
-def clean_data(deletion_candidates, schedule_candidates):
+def clean_data(deletion_candidates, schedule_candidates, **kwargs):
+    retry_post = kwargs.get("retry_post", RETRY_POST)
     # delete deletion candidate model objects
     if deletion_candidates.exists():
         log_message = message("LOG_EVENT", event='Objects "%s" have been deleted' % string_list(get_values_list("pk", queryset=deletion_candidates, unique=False)))
         deletion_candidates.delete()
         logger.info(log_message)
-    # schedule schedule candidate model objects
+    # schedule or delete schedule candidate model objects
     if schedule_candidates.exists():
-        for candidate in schedule_candidates:
-            log_message = message("LOG_EVENT", event='Scheduling object "%s" that was left behind' % candidate.pk)
+        if not retry_post:
+            verbose_event = 'Objects "%s" that were left behind have been deleted' % string_list(get_values_list("pk", queryset=schedule_candidates, unique=False))
+            log_message = message("LOG_EVENT", event=verbose_event)
+            schedule_candidates.delete()
             logger.info(log_message)
-            schedule_post(candidate)
+        else:
+            for candidate in schedule_candidates:
+                log_message = message("LOG_EVENT", event='Scheduling object "%s" that was left behind' % candidate.pk)
+                logger.info(log_message)
+                schedule_post(candidate)
