f637cdb65ef7252afab7a31ddd46688d90524053
irfanhakim-as <irfanhakim.as@yahoo.com>
2024-01-10 01:12:56 +0800

    Continue scheduler run to the next account
    Instead of exiting the scheduler entirely

--- base/scheduler.py
+++ base/scheduler.py
@@ -130,7 +130,7 @@ def post_scheduler(pending_objects, updating_objects, **kwargs):
                     verbose_error = 'Post "%s" (%s) has not successfully returned an ID' % (post_object, account_id)
                     log_error = message("LOG_EXCEPT", exception=None, verbose=verbose_error, object=post_object)
                     logger.error(log_error)
-                    return
+                    continue
                 pid = "%s_%s" % (account_id, post_id)
                 # update subject object post_id if new or non-format conforming post
                 if not account_pid:
