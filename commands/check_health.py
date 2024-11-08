b91690bbde9f642566d59678cbea2eb77fc9c7ce
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-11-08 19:11:44 +0800

    all: Update command help message to be more generic

--- commands/check_health.py
+++ commands/check_health.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from lib.mastodon import check_health
 
 class Command(BaseCommand):
-    help = "Checks the health of the Mastodon bot by sending a test post"
+    help = "Checks the health of the bot by sending a test post"
 
     def handle(self, *args, **options):
         check_health()
