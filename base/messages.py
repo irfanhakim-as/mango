04fdb862aa62bdfda476da967ec523e2acfa9844
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-11-08 19:07:23 +0800

    bsky: Add BLUESKY_TEST test message

--- base/messages.py
+++ base/messages.py
@@ -7,5 +7,6 @@ MESSAGES = {
     "LOG_EVENT" : "EVENT={event}",
     "LOG_EXCEPT" : "EXCEPTION={exception} VERBOSE={verbose} OBJECT={object}",
     "FEED_POST" : "{title}{tags}{link}",
+    "BLUESKY_TEST" : "This is an automated post from @{id} to test the health of the bot.",
     "MASTODON_TEST" : "This is an automated, {visibility} post from @{name} to test the health of the bot.",
 }
