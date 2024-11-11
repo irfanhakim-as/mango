27b550d25cd347f6b49306b5791ed7d1a1b7952f
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-11-11 14:33:52 +0800

    all: Trigger check_health for bsky as well

--- commands/entrypoint.py
+++ commands/entrypoint.py
@@ -1,10 +1,12 @@
 from django.core.management.base import BaseCommand
 from base.methods import sync_data
-from lib.mastodon import check_health
+from lib.bluesky import check_health as check_bluesky_health
+from lib.mastodon import check_health as check_mastodon_health
 
 class Command(BaseCommand):
     help = "Entrypoint script"
 
     def handle(self, *args, **options):
         sync_data()
-        check_health()
+        check_bluesky_health()
+        check_mastodon_health()
