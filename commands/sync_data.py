b828bec6645a2583e1cde68e15f7740084311310
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-11-13 21:52:23 +0800

    all: Remove period from help message

--- commands/sync_data.py
+++ commands/sync_data.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from base.methods import sync_data
 
 class Command(BaseCommand):
-    help = "Sync essential models with data from JSON files."
+    help = "Sync essential models with data from JSON files"
 
     def handle(self, *args, **options):
         sync_data()
