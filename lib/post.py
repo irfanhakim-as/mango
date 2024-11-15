bad8c0a2b2d1627730f4928830274081dd3ce68f
Irfan Hakim <irfanhakim.as@yahoo.com>
2024-11-15 12:02:30 +0800

    all: Explicitly exclude deletion candidates from schedule candidates
    Post objects that are meant to be deleted should not be scheduled for posting

--- lib/post.py
+++ lib/post.py
@@ -1,4 +1,7 @@
-from base.methods import get_post_model
+from base.methods import (
+    get_post_model,
+    get_values_list,
+)
 from base.post import clean_data as _clean_data
 PostModel = get_post_model()
 
@@ -8,7 +11,7 @@ def clean_data(**kwargs):
     # populate deletion candidates with model objects that have been posted and not currently being scheduled
     deletion_candidates = PostModel.objects.filter(post_id__isnull=False, postschedule__isnull=True)
 
-    # populate schedule candidates with model objects that have neither been posted nor scheduled
-    schedule_candidates = PostModel.objects.filter(post_id__isnull=True, postschedule__isnull=True)
+    # populate schedule candidates with model objects that have neither been posted nor scheduled and not among the deletion candidates
+    schedule_candidates = PostModel.objects.filter(post_id__isnull=True, postschedule__isnull=True).exclude(pk__in=get_values_list("pk", queryset=deletion_candidates))
 
     _clean_data(deletion_candidates, schedule_candidates, **kwargs)
