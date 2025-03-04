80ef25cb1b33d89119d4b3b4db7e2c01a15f3fae
Irfan Hakim <irfanhakim.as@yahoo.com>
2025-03-05 01:04:30 +0800

    Reuse method in check_db command

--- commands/check_db.py
+++ commands/check_db.py
@@ -1,14 +1,13 @@
 import sys
-from django.db import connection
 from django.core.management.base import BaseCommand
+from base.methods import check_db
 
 class Command(BaseCommand):
     help = "Check database connection"
 
     def handle(self, *args, **options):
-        try:
-            connection.ensure_connection()
-        except Exception as e:
-            self.stderr.write(self.style.ERROR("Database connection failed."))
+        response = check_db()
+        if response.status_code != 200:
+            self.stderr.write(self.style.ERROR(response.content.decode()))
             sys.exit(1)
-        self.stdout.write(self.style.SUCCESS("Database connection successful."))
+        self.stdout.write(self.style.SUCCESS(response.content.decode()))
